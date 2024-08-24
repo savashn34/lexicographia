258a75c2399770e7e7a7672896c163d5f89cdeeb
savashn34 <savashn34@gmail.com>
2024-08-24 03:00:50 +0300

    update

--- src/app/template.js
+++ src/app/template.js
@@ -10,7 +10,7 @@ export default function RootTemplate({ children }) {
 
                 <div className={`${styles.footer}`}>
                     <p>
-                        lexicographia.org
+                        Lexicographia
                     </p>
 
                     <br />
